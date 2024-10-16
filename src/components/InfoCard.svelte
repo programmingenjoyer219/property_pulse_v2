0c82aa2343966f464b0e7f0aa21f761eee2088af
Yuganshu <yuganshu219@gmail.com>
2024-10-16 17:35:06 +0530

    create info cards and add them to home page

---
+++ src/components/InfoCard.svelte
@@ -0,0 +1,14 @@
+<script lang="ts">
+	export let title: string;
+	export let content: string;
+</script>
+
+<div class="rounded-md p-6 shadow-md bg-gray-100">
+	<h2 class="text-2xl font-bold text-gray-800 mb-4 sm:text-3xl">
+		{title}
+	</h2>
+	<p class="mb-6 sm:text-lg">
+		{content}
+	</p>
+	<slot></slot>
+</div>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,5 +1,8 @@
 <script>
 	import Hero from "$components/Hero.svelte";
+	import InfoCard from "$components/InfoCard.svelte";
+
+	let loggedIn = false;
 </script>
 
 <svelte:head>
@@ -15,3 +18,31 @@
 </svelte:head>
 
 <Hero />
+
+<section class="section">
+	<div class="container-mod">
+		<div class="grid grid-rows-2 gap-6 md:gap-10 md:grid-rows-1 md:grid-cols-2">
+			<!-- for renters -->
+			<InfoCard
+				title={"For Renters"}
+				content={"Find your dream rental property. Bookmark properties and contact owners."}
+			>
+				<a href="/properties" class="button-primary">Browse Properties</a>
+			</InfoCard>
+			<!-- for property owners -->
+			<InfoCard
+				title={"For Property Owners"}
+				content={"List your properties and reach potential tenants. Rent as an airbnb or a long term rental."}
+			>
+				{#if loggedIn}
+					<a href="/add" class="button-primary">Add Property</a>
+				{:else}
+					<a href="/add" class="button-primary space-x-1">
+						<i class="ri-google-fill"></i>
+						<span>Login To Add Properties</span>
+					</a>
+				{/if}
+			</InfoCard>
+		</div>
+	</div>
+</section>
