8041fc554d0f080ec8c2f450f3a6edfe3931581d
Yuganshu <yuganshu219@gmail.com>
2024-10-18 20:38:32 +0530

    remove navigation links

--- src/components/Navbar.svelte
+++ src/components/Navbar.svelte
@@ -12,63 +12,6 @@
 	</a>
 
 	<div class="flex items-center gap-2 sm:gap-4">
-		<!-- links to different pages, visible on large screens -->
-		<nav class="hidden sm:flex items-center gap-2">
-			<!-- home -->
-			<a href="/" class="{$page.route.id === '/' && 'bg-blue-800'} nav-link">
-				<i class="ri-home-7-fill text-xl"></i>
-				<span>Home</span>
-			</a>
-			<!-- properties -->
-			<a
-				href="/properties"
-				class="{$page.route.id?.includes('/properties') &&
-					'bg-blue-800'} nav-link"
-			>
-				<i class="ri-building-fill text-xl"></i>
-				<span>Properties</span>
-			</a>
-		</nav>
-
-		<!-- links to different pages, visible on small screens -->
-		<div class="sm:hidden dropdown dropdown-end relative">
-			<div tabindex="0" role="button" class="btn btn-ghost btn-circle avatar">
-				<!-- hamburger icon -->
-				<div
-					class="w-10 rounded-full border-2 border-blue-400 hover:border-blue-100 transition-colors duration-200 bg-blue-700 hover:bg-blue-800 text-gray-50 grid place-content-center"
-				>
-					<i class="ri-menu-line text-xl"></i>
-				</div>
-
-				<!-- links -->
-				<ul
-					class="absolute top-[3rem] menu menu-sm dropdown-content bg-blue-700 border-2 border-white rounded-md z-[1] w-52 p-1 shadow-md flex flex-col gap-1"
-				>
-					<!-- home -->
-					<li>
-						<a
-							href="/"
-							class="{$page.route.id?.includes('/') && 'bg-blue-800'} nav-link"
-						>
-							<i class="ri-home-7-fill text-xl"></i>
-							<span>Home</span>
-						</a>
-					</li>
-					<!-- properties -->
-					<li>
-						<a
-							href="/properties"
-							class="{$page.route.id?.includes('/properties') &&
-								'bg-blue-200'} nav-link"
-						>
-							<i class="ri-building-fill text-xl"></i>
-							<span>Properties</span>
-						</a>
-					</li>
-				</ul>
-			</div>
-		</div>
-
 		<!-- user profile -->
 		<div class="dropdown dropdown-end relative">
 			<div tabindex="0" role="button" class="btn btn-ghost btn-circle avatar">
