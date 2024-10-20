0aaad911759cc9efb4a2446dc7603cbea5dd7820
Yuganshu <yuganshu219@gmail.com>
2024-10-20 17:27:53 +0530

    create custom header component

---
+++ src/components/CustomHeader.svelte
@@ -0,0 +1,7 @@
+<section class="section bg-blue-700">
+	<div class="container-mod-small space-y-6">
+		<div class="space-y-4 text-center text-gray-50 flex flex-col items-center">
+			<slot></slot>
+		</div>
+	</div>
+</section>
